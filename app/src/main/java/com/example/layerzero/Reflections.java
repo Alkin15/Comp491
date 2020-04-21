fc803e7cf5e7431187cf3f4ee3b50223e1bdd426
Alkin <alkinkir@gmail.com>
2020-04-22 00:26:44 +0300

    UI revisited, Firebase

---
+++ app/src/main/java/com/example/layerzero/Reflections.java
@@ -0,0 +1,31 @@
+package com.example.layerzero;
+
+public class Reflections {
+    private String sensory;
+    private String emotional;
+    private String brief;
+
+    public String getSensory() {
+        return sensory;
+    }
+
+    public void setSensory(String sensory) {
+        this.sensory = sensory;
+    }
+
+    public String getEmotional() {
+        return emotional;
+    }
+
+    public void setEmotional(String emotional) {
+        this.emotional = emotional;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+}
--- app/src/main/java/com/example/layerzero/SensoryMainActivity.java
+++ app/src/main/java/com/example/layerzero/SensoryMainActivity.java
@@ -1,5 +1,6 @@
 package com.example.layerzero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,16 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.example.layerzero.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SensoryMainActivity extends AppCompatActivity {
+    DatabaseReference reff;
+    Reflections reflection;
+    long reflection_num;
     public static final String EXTRA_MESSAGE = "com.example.Survey.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,24 @@ public class SensoryMainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.briefResponse);
         String response = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE,response);
+        reflection = new Reflections();
+        reflection.setBrief(response);
+
+        reff = FirebaseDatabase.getInstance().getReference().child("Reflections");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        reflection_num = dataSnapshot.getChildrenCount();
+                    }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        reff.child(String.valueOf(reflection_num+1)).setValue(reflection);
         startActivity(intent);
     }
 }
