e02092f173d6ac9e016ab8e7aff8163c3e1033a0
Pelin <pdurak15@ku.edu.tr>
2020-04-29 14:07:09 +0300

    Added Firebase

--- app/src/main/java/com/example/layerzero/ReflectionLog.java
+++ app/src/main/java/com/example/layerzero/ReflectionLog.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ReflectionLog extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class ReflectionLog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflection_log);
+
+        Toast.makeText(ReflectionLog.this, "Fiebase connected", Toast.LENGTH_LONG).show();
 
         String sensoryContent =
                 "<h1 style=\"text-align:center; color:black\">Sensory Significance</h1>\n" +
