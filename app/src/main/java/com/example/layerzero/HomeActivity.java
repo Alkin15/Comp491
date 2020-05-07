14c59f485440587ec518268ec856a4667ce8de54
ENVER CAN KAYANDAN <ekayandan16@ku.edu.tr>
2020-05-07 16:41:14 +0300

    started login screen

--- app/src/main/java/com/example/layerzero/HomeActivity.java
+++ app/src/main/java/com/example/layerzero/HomeActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Switch;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.ArrayList;
 
@@ -20,6 +23,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.v("tag", FirebaseInstanceId.getInstance().getInstanceId().toString());
         sButton = findViewById(R.id.buttonS);
         sButton.setOnClickListener(new View.OnClickListener() {
             @Override
