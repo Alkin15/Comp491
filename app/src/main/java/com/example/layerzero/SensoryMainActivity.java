a87fccad9f70596ca0580aa8f357e253c6f19e54
ENVER CAN KAYANDAN <ekayandan16@ku.edu.tr>
2020-05-21 15:23:01 +0300

    gallery add

--- app/src/main/java/com/example/layerzero/SensoryMainActivity.java
+++ app/src/main/java/com/example/layerzero/SensoryMainActivity.java
@@ -5,16 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class SensoryMainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.Survey.MESSAGE";
 
+    public static final int PICK_IMAGE = 1;
+    private Button galleryButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensory_main);
+        galleryButton = findViewById(R.id.gallery_button);
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+            }
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+
+        }
+    }
+
 
     public void sendMessage(View view){
         Intent intent = new Intent(this, sensoryActivity.class);
